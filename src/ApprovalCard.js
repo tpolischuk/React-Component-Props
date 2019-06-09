be34badd5891d6fc7d26c2585f436dce61ca1d61
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-06-09 14:17:31 -0700

    Children components

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
+
+  console.log(props.children)
+
   return (
     <div className="ui card">
       <div className="content">
-        Are you sure?
+      {props.children}
       </div>
       <div className="extra content">
+        <div className="ui two buttons">
         <div className="ui basic green button">Approve</div>
-      <div className="ui basic red button">Reject</div>
+        <div className="ui basic red button">Reject</div>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -7,25 +7,38 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author={faker.internet.userName()}
-        imageAvatar={faker.image.avatar()}
-        timeAgo="Today at 4:45PM"
-        content={faker.lorem.words()}
-        />
-      <CommentDetail
-        author={faker.internet.userName()}
-        imageAvatar={faker.image.avatar()}
-        timeAgo="Today at 2:00AM"
-        content={faker.lorem.words()}
-        />
-      <CommentDetail
-        author={faker.internet.userName()}
-        imageAvatar={faker.image.avatar()}
-        timeAgo="Yesterday at 5:03PM"
-        content={faker.lorem.words()}
-        />
-      <ApprovalCard />
+
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          imageAvatar={faker.image.avatar()}
+          timeAgo="Today at 4:45PM"
+          content={faker.lorem.words()}
+          />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          imageAvatar={faker.image.avatar()}
+          timeAgo="Today at 2:00AM"
+          content={faker.lorem.words()}
+          />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.internet.userName()}
+          imageAvatar={faker.image.avatar()}
+          timeAgo="Yesterday at 5:03PM"
+          content={faker.lorem.words()}
+          />
+      </ApprovalCard>
     </div>
   );
 };
